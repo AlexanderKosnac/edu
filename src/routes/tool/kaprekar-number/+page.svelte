2f1453f17bd80908045a6554e9a865c0c20c2b62
Alexander Kosnac <alexander.kosnac@web.de>
2024-04-28 23:12:25 +0200

    Start Kaprekar number tool

---
+++ src/routes/tool/kaprekar-number/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<div class="row">
+    <div class="col">
+        <h1>Kaprekar number</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        In work
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Kaprekar_number" target="_blank">Kaprekar number</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
