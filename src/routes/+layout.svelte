ef793b678b0d88dd6ac5b97295b5238984af6109
Alexander Kosnac <alexander.kosnac@web.de>
2024-06-29 01:25:53 +0200

    Disable drag on header favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,8 @@ import ThemeToggle from '$lib/ThemeToggle/ThemeToggle.svelte';
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="{base}/">
-			<img src="{base}/favicon.svg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+		<a class="navbar-brand" href="{base}/" draggable="false">
+			<img src="{base}/favicon.svg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top" draggable="false">
 		</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		  <span class="navbar-toggler-icon"></span>
