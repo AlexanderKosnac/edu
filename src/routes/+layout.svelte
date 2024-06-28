7ee905b7f4f214cab409a298e92216e36c957148
Alexander Kosnac <alexander.kosnac@web.de>
2024-06-28 23:07:27 +0200

    Display available tools on index site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,7 @@ import ThemeToggle from '$lib/ThemeToggle/ThemeToggle.svelte';
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<!--li class="nav-item"><a class="nav-link" aria-current="page" href="{base}/tool">Tools</a></li-->
 			</ul>
 			<div class="nav-item"><ThemeToggle/></div>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import data from "./tool/overview-data.json";
+
+    const sortedData = data.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+</script>
+
+<h1><tt>EDU</tt></h1>
+
+<p>
+    EDU is a collection of tools, visualizations, and implementations with a focus on education and learning.
+</p>
+
+<p>
+    This website is hosted on GitHub pages, automatically deployed from the <a href="https://github.com/AlexanderKosnac/edu">edu</a> repository.
+</p>
+
+<div class="d-flex flex-wrap gap-2">
+  {#each sortedData as entry}
+  <div class="card tool-card">
+    <div class="card-body">
+      <h5 class="card-title">
+        <a class="icon-link icon-link-hover" href="{entry.url}">
+            {entry.name}
+            <i class="bi bi-arrow-right-short"></i>
+        </a>
+    </h5>
+      <p class="card-text">{entry.description}</p>
+    </div>
+  </div>
+  {/each}
+</div>
+
+<style>
+.tool-card {
+  width: 18rem;
+  min-width: 18rem;
+}
+</style>
