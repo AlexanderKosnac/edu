d7adb020a15a74ec48ccb7382aac82ddcdb7d8df
Alexander Kosnac <alexander.kosnac@web.de>
2024-07-31 21:19:47 +0200

    Perspective projection

--- src/routes/tool/projections/+page.svelte
+++ src/routes/tool/projections/+page.svelte
@@ -27,6 +27,19 @@
             [field(0),       field(0),       field(-2/(f-n)), field(-(f+n)/(f-n))],
             [field(0),       field(0),       field(0),        field(1)],        
         ]}/>
+
+        <h2>Perspective Projection</h2>
+        <Projection displayMatrix={[
+            [field("2n/(r-l)"), field(0),          field("(r+l)/(r-l)"),  field(0)],
+            [field(0),          field("2n/(t-b)"), field("(t+b)/(t-b)"),  field(0)],
+            [field(0),          field(0),          field("-(f+n)/(f-n)"), field("-2fn/(f-n)")],
+            [field(0),          field(0),          field(-1),             field(0)],        
+        ]} discreteMatrix={(r, l, t, b, n, f) => [
+            [field(2*n/(r-l)), field(0),         field((r+l)/(r-l)),        field(0)],
+            [field(0),         field(2*n/(t-b)), field((t+b)/(t-b)),        field(0)],
+            [field(0),         field(0),         field(-(f+n)/(f-n)), field(-2*f*n/(f-n))],
+            [field(0),         field(0),         field(-1),        field(0)],        
+        ]}/>
     </div>
 </div>
 
@@ -35,6 +48,7 @@
         <h2>References:</h2>
         <ul>
             <li><a href="https://www.songho.ca/opengl/gl_projectionmatrix.html" target="_blank">Article on OpenGL Projectionmatrices</a></li>
+            <li><a href="http://learnwebgl.brown37.net/08_projections/projections_perspective.html" target="_blank">Article on perspective projection</a></li>
         </ul>
     </div>
 </div>
