0ec216e1f7d994169d737b238fab43dfbc5656da
Alexander Kosnac <alexander.kosnac@web.de>
2024-08-04 23:56:22 +0200

    Rework landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,38 +8,53 @@
     });
 </script>
 
-<h1><tt style="letter-spacing: .4rem">edu</tt></h1>
+<svelte:head>
+    <title>edu</title>
+</svelte:head>
 
-<p>
-  EDU is a collection of tools, visualizations, and calculators with a focus on education and learning.
-  The name is derived from top-level domain <a href="https://en.wikipedia.org/wiki/.edu">.edu</a> which was created for organizations with a focus on education.
-</p>
+<div class="row flex-row justify-content-center">
+  <div class="col-lg-6">
+    <div class="d-flex flex-column align-items-center">
+      <h1 class="text-center"><tt style="letter-spacing: .2rem">edu</tt></h1>
 
-<p>
-  This website is hosted on GitHub pages, automatically deployed from the <a href="https://github.com/AlexanderKosnac/edu">edu</a> repository.
-</p>
-
-<div class="d-flex flex-wrap gap-2">
-  {#each sortedData as entry}
-  {#if entry.usable ?? true}
-  <div class="card tool-card">
-    <div class="card-body">
-      <h5 class="card-title">
-        <a class="icon-link icon-link-hover" href="{entry.url}">
-          {entry.name}
-          <i class="bi bi-arrow-right-short"></i>
-        </a>
-    </h5>
-      <p class="card-text">{entry.description}</p>
+      <p>
+        The edu project is a collection of tools, visualizations, and calculators with a focus on education and learning.
+        The name is derived from the top-level domain <a href="https://en.wikipedia.org/wiki/.edu">.edu</a> which was created for organizations with a focus on education.
+      </p>
+      
+      <p>
+        This website is hosted on GitHub pages, automatically deployed from the <a href="https://github.com/AlexanderKosnac/edu">edu</a> repository.
+        I do not guarantee the accuracy or correctness of the included tools or information.
+      </p>
     </div>
   </div>
-  {/if}
-  {/each}
+</div>
+
+<div class="row justify-content-center">
+  <div class="col-lg-10">
+    <div class="d-flex flex-wrap justify-content-center gap-2">
+      {#each sortedData as entry}
+      {#if entry.usable ?? true}
+      <div class="card tool-card">
+        <div class="card-body">
+          <h5 class="card-title">
+            <a class="icon-link icon-link-hover" href="{entry.url}">
+              {entry.name}
+              <i class="bi bi-arrow-right-short"></i>
+            </a>
+        </h5>
+          <p class="card-text">{entry.description}</p>
+        </div>
+      </div>
+      {/if}
+      {/each}
+    </div>    
+  </div>
 </div>
 
 <style>
-.tool-card {
-  width: 18rem;
-  min-width: 18rem;
-}
+  .tool-card {
+    width: 280px;
+    min-width: 280px;
+  }
 </style>
