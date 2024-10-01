b30284088724034263360cce7375ce74b09f02c1
Alexander Kosnac <alexander.kosnac@web.de>
2024-10-01 23:03:21 +0200

    Basic structure for rendering pipeline outline

---
+++ src/routes/tool/rendering-pipeline/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Rendering Pipeline</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Rendering Pipeline</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        An implementation of a basic computer graphics rendering pipeline can be found in the following repository.
+        <a href="https://github.com/AlexanderKosnac/renderer" target="_blank">github.com/AlexanderKosnac/renderer</a>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Graphics_pipeline" target="_blank">Graphics pipeline</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
