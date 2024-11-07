a70b143312b04decfcaf01d9cba57d70a680fb71
Alexander Kosnac <alexander.kosnac@web.de>
2024-11-07 23:41:08 +0100

    Implement basic base64 encoding visualization

--- src/routes/tool/base64/+page.svelte
+++ src/routes/tool/base64/+page.svelte
@@ -8,11 +8,38 @@
         "8", "9", "+", "/"
     ]
 
-    let inputElement;
-    let outputElement;
+    let inputString = "Ma";
+
+    let chars = [];
+    let octets = [];
+    let bits = [];
+    let sextetsEnc = [];
+    let charsEnc = [];
+    let octetsEnc = [];
+
+    $: outputString = charsEnc.join("");
 
     function encodeData() {
-        // implement
+        chars = [];
+        octets = [];
+        bits = [];
+        sextetsEnc = [];
+        charsEnc = [];
+        octetsEnc = [];
+        for (let i = 0; i < inputString.length; i++) {
+            const c = inputString.charAt(i);
+            chars.push(c);
+            octets.push(c.charCodeAt(0));
+            bits.push(...c.charCodeAt(0).toString(2).padStart(8, "0").split(""));
+        }
+        if (bits.length%6) bits.push(...Array(6-bits.length%6).fill("0")); // Padding to have a multiple of 6
+        for (let i = 0; i < bits.length; i+=6) {
+            const encoded = parseInt(bits.slice(i, i+6).join(""), 2);
+            const c = characters[encoded];
+            sextetsEnc.push(encoded);
+            charsEnc.push(c);
+            octetsEnc.push(c.charCodeAt(0));
+        }
     }
 </script>
 
@@ -30,14 +57,65 @@
     <div class="col">
         Unencoded Input String:
         <div class="d-flex flex-row gap-1">
-            <input type="text" class="form-control font-monospace" bind:value="{inputElement}">
+            <input type="text" class="form-control font-monospace" bind:value="{inputString}" on:input={encodeData}>
             <button type="button" class="btn btn-primary" on:click={encodeData}>Encode</button>
         </div>
 
         <div class="form-group">
             <label for="outputElement">Base64 Encoded Data:</label>
-            <textarea class="form-control font-monospace" id="outputElement" readonly rows="3" bind:this={outputElement}></textarea>
+            <textarea class="form-control font-monospace" id="outputElement" readonly rows="3" bind:value={outputString}></textarea>
         </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        Detailed Output:
+        <table class="table table-bordered w-auto">
+            <tbody>
+                <tr>
+                    <th rowspan="2">Source<br>ASCII text</th>
+                    <th>Character</th>
+                    {#each chars as c}
+                        <td colspan="8"><strong>{c}</strong></td>
+                    {/each}
+                    {#if (8*chars.length)%6}
+                        <td rowspan="2" colspan="{6-(8*chars.length)%6}">{6-(8*chars.length)%6}</td>
+                    {/if}
+                </tr>
+                <tr>
+                    <th>Octets</th>
+                    {#each octets as o}
+                        <td colspan="8">{o} (0x{o.toString(16)})</td>
+                    {/each}
+                </tr>
+                <tr>
+                    <th colspan="2">Bits</th>
+                    {#each bits as b}
+                        <td>{b}</td>
+                    {/each}
+                </tr>
+                <tr>
+                    <th rowspan="3">Base64<br>encoded</th>
+                    <th>Sextets</th>
+                    {#each sextetsEnc as se}
+                    <td colspan="6">{se}</td>
+                    {/each}
+                </tr>
+                <tr>
+                    <th>Character</th>
+                    {#each charsEnc as ce}
+                        <td colspan="6"><strong>{ce}</strong></td>
+                    {/each}
+                </tr>
+                <tr>
+                    <th>Octets</th>
+                    {#each octetsEnc as oe}
+                        <td colspan="6">{oe} (0x{oe.toString(16)})</td>
+                    {/each}
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 
@@ -80,4 +158,8 @@
 </div>
 
 <style>
+    th, td {
+        vertical-align: middle;
+        text-align: center;
+    }
 </style>
