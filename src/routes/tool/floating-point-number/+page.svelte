12efc7a7ad41a0c0936e504edf675a3958d28b9a
Alexander Kosnac <alexander.kosnac@web.de>
2024-11-12 23:57:47 +0100

    Function to parse binary numbers with fractionals

--- src/routes/tool/floating-point-number/+page.svelte
+++ src/routes/tool/floating-point-number/+page.svelte
@@ -34,6 +34,21 @@
             throwOnError: false
         });
     }
+
+    function parseBinary(string) {
+        const [integerPart, fractionalPart] = string.split(".");
+
+        let integerDecimal = parseInt(integerPart, 2);
+
+        let fractionalDecimal = 0;
+        if (fractionalPart) {
+            for (let i = 0; i < fractionalPart.length; i++) {
+                fractionalDecimal += parseInt(fractionalPart[i], 2) * Math.pow(2, -(i + 1));
+            }
+        }
+
+        return integerDecimal + fractionalDecimal;
+    }
 </script>
 
 <svelte:head>
