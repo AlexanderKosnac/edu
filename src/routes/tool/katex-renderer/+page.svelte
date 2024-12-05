3144f1b209b1390d5cb8626438fc3d2280432e15
Alexander Kosnac <alexander.kosnac@web.de>
2024-12-05 23:19:00 +0100

    KaTeX renderer tool

---
+++ src/routes/tool/katex-renderer/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    import katex from "katex";
+    import "katex/dist/katex.min.css";
+
+    let katexInput = "Q_n(f) := \\int_a^b p_n(x) \\, dx = \\sum_{i=0}^n \\underbrace{\\left( \\int_a^b L_i^n(x_i) \\, dx \\right)}_{=:w_i} f(x_i)"; // Example is: Interpolatorische Quadraturformeln
+    $: katexResult = katex.renderToString(katexInput, {
+            throwOnError: false,
+            displayMode: true, // Sets non-inline display
+    });
+
+</script>
+
+<svelte:head>
+    <title>KaTeX Renderer</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>KaTeX Renderer</h1>
+    </div>
+</div>
+
+<div class="row pb-5">
+    <div class="col">
+        <div class="form-group">
+            <label for="inputText">KaTeX Math Expression:</label>
+            <input type="text" class="form-control font-monospace" bind:value="{katexInput}">
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <div class="d-flex justify-content-center">
+            <div class="latex-result">{@html katexResult}</div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://www.npmjs.com/package/katex" target="_blank">KaTeX Library on npm</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+    .latex-result {
+        font-size: 2.5em;
+        padding: 10px;
+    }
+</style>
