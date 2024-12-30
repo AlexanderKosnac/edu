d534e35e5f6b523d05198ddb315dfc8895de2a4a
Alexander Kosnac <alexander.kosnac@web.de>
2024-12-30 18:43:54 +0100

    Base frame for randomizer tools

---
+++ src/routes/tool/randomizer/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Randomizer</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Randomizer</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
