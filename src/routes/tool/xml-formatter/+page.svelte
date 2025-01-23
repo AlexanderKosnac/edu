ec67e772f83a6a3b2603bd8f383bfecc90b4cee3
Alexander Kosnac <alexander.kosnac@web.de>
2025-01-23 23:15:51 +0100

    Frame for xml formatter

---
+++ src/routes/tool/xml-formatter/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(()=> {
+    });
+</script>
+
+<svelte:head>
+    <title>XML Formatter</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>XML Formatter</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://www.npmjs.com/package/xml-formatter" target="_blank">xml-formatter npm package</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
