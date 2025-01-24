6e154f6f6991f17ff9703bc1eafba5235f19bf44
Alexander Kosnac <alexander.kosnac@web.de>
2025-01-24 22:04:25 +0100

    Implement XML formatter

--- src/routes/tool/xml-formatter/+page.svelte
+++ src/routes/tool/xml-formatter/+page.svelte
@@ -1,8 +1,36 @@
 <script>
-    import { onMount } from "svelte";
+    import xmlFormat from 'xml-formatter';
 
-    onMount(()=> {
-    });
+    let xmlString = `\
+<Person>
+    <Name>George Washington</Name>
+    <Birth>
+        <Date>1732-02-22</Date>
+        <Place>Westmoreland County, Virginia</Place>
+    </Birth>
+    <Roles>
+        <Role>General</Role>
+        <Role>President</Role>
+    </Roles>
+    <Presidency>
+        <TermStart>1789-04-30</TermStart>
+        <TermEnd>1797-03-04</TermEnd>
+        <Number>1</Number>
+    </Presidency>
+</Person>`;
+    $: xmlPretty = (() => {
+        try {
+            return xmlFormat(xmlString, {
+                indentation: '  ', 
+                filter: (node) => node.type !== 'Comment', 
+                collapseContent: true, 
+                lineSeparator: '\n'
+            });
+        } catch (err) {
+            return `Invalid XML. ${err}`;
+        }
+    })();
+
 </script>
 
 <svelte:head>
@@ -17,6 +45,15 @@
 
 <div class="row">
     <div class="col">
+        <div class="form-group">
+            <label for="inputText">XML Input:</label>
+            <textarea class="form-control font-monospace" id="inputText" rows="20" bind:value="{xmlString}"></textarea>
+        </div>
+    </div>
+
+    <div class="col">
+        <label for="outputText">XML Formatted:</label>
+        <textarea class="form-control font-monospace" id="outputText" rows="20" bind:value="{xmlPretty}" readonly="true"></textarea>
     </div>
 </div>
 
