e05fd917d0e7771ed2fcdd20fed7d622c3a49ac0
Alexander Kosnac <alexander.kosnac@web.de>
2025-02-15 21:38:47 +0100

    Frame for hamming code tool

---
+++ src/routes/tool/hamming-code/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Hamming Codes</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Hamming Codes</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Hamming_code" target="_blank">Hamming Codes on Wikipedia</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
