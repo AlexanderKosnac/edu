a2cef798a09b793b87ce1fb1d4954a1fa1cbbabb
Alexander Kosnac <alexander.kosnac@web.de>
2025-02-16 21:20:10 +0100

    Input for Hamming code tool

--- src/routes/tool/hamming-code/+page.svelte
+++ src/routes/tool/hamming-code/+page.svelte
@@ -1,4 +1,22 @@
 <script>
+    let inputAscii = "Hamming";
+
+    function charsAsBinaryDumpLines(input) {
+        const padding = `${input.length}`.length;
+        return input.split("").map((c, i) => `${i.toString().padStart(padding, " ")}  ${c.charCodeAt(0).toString(2).padStart(8, "0")}  ${c}`);
+    }
+
+    function charsAsBinaryList(input) {
+        return Array.from(input).flatMap(c => byteAsBinaryList(c.charCodeAt(0)));
+    }
+
+    function byteAsBinaryList(byte) {
+        return byte.toString(2).padStart(8, "0").split("").map(bit => Number(bit));
+    }
+
+    function onInputChanged() {
+        charsAsBinaryDumpLines()
+    }
 </script>
 
 <svelte:head>
@@ -11,9 +29,21 @@
     </div>
 </div>
 
+<div class="row mb-1">
+    <div class="col">
+        <label for="asciiInput">ASCII Input:</label>
+        <input type="text" id="asciiInput" class="form-control font-monospace" bind:value="{inputAscii}" on:input={onInputChanged}/>
+    </div>
+</div>
+
 <div class="row">
     <div class="col">
 
+    </div>
+
+    <div class="col">
+        <h3>Input as Binary Data:</h3>
+        <pre>{#each charsAsBinaryDumpLines(inputAscii) as bitsString}<span class="font-monospace">{bitsString}</span><br>{/each}</pre>
     </div>
 </div>
 
