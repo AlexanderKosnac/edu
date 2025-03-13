a0888698a6d7d68f2955c2b0b95ea5d95c52b08f
Alexander Kosnac <alexander.kosnac@web.de>
2025-03-13 21:35:17 +0100

    Restructure sort and unique UI

--- src/routes/tool/sort-and-unique/+page.svelte
+++ src/routes/tool/sort-and-unique/+page.svelte
@@ -3,51 +3,51 @@
     let splitCharacter = "\\n";
     let makeUnique = true;
 
-    let inputElements = [] 
+    let inputElements = [];
     let outputElements = [];
+    $: outputText = outputElements.join(splitCharacter);
 
     function run() {
-        const c = splitCharacter.replace(/\\n/g, '\n').replace(/\\t/g, '\t');
+        const c = splitCharacter.replace(/\\n/g, "\n").replace(/\\t/g, "\t");
         inputElements = inputText.split(c);
-        outputElements = [... inputElements];
+        outputElements = [...inputElements];
         if (makeUnique) {
             outputElements = [...new Set(outputElements)];
         }
         outputElements = outputElements.sort();
-        outputElements = elements;
     }
 </script>
 
 <div class="row">
-    <div>
-        A basic tool that allows to sort and extract unique elements from a text.
-    </div>
-    <div class="col">
+    <div class="col-12 col-md-auto">
         <h2>Input</h2>
         <div class="d-flex flex-column gap-1 mb-1">
             <lable>
                 Split on
-                <input type="text" class="form-control font-monospace" bind:value="{splitCharacter}"/>
+                <input type="text" class="form-control font-monospace" bind:value={splitCharacter} />
             </lable>
 
             <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" bind:checked={makeUnique}/>
+                <input class="form-check-input" type="checkbox" bind:checked={makeUnique} />
                 Only show unique elements
             </label>
-            
+
             <button type="button" class="btn btn-primary" onclick={run}>Extract</button>
+
+            <strong>Statistics:</strong>
+            <ul>
+                <li>Number of input elements: {inputElements.length}</li>
+                <li>Number of output elements: {outputElements.length}</li>
+            </ul>
         </div>
-        <textarea class="form-control" rows="20" bind:value="{inputText}"></textarea>
     </div>
-    <div class="col">
+    <div class="col-12 col-md">
+        <h2>Input</h2>
+        <textarea class="form-control" rows="20" bind:value={inputText}></textarea>
+    </div>
+    <div class="col-12 col-md">
         <h2>Output</h2>
-        <ul>
-            Number of input elements: {inputElements.length}<br>
-            Number of output elements: {outputElements.length}<br>
-            {#each outputElements as e}
-                <li>{e}</li>
-            {/each}
-        </ul>
+        <textarea class="form-control" rows="20" bind:value={outputText}></textarea>
     </div>
 </div>
 
