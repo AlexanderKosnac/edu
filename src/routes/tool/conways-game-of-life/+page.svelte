551f0ce1769ec6c51e45cac123ffe9645fd2bff4
Alexander Kosnac <alexander.kosnac@web.de>
2025-04-02 22:56:35 +0200

    Frame for Conways Game of Life

---
+++ src/routes/tool/conways-game-of-life/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Conway's Game of Life</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">Conway's Game of Life (Wikipedia)</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
