f8c47c1a478c234f80cfafbd2a934a57c68ed5ce
Alexander Kosnac <alexander.kosnac@web.de>
2025-04-24 22:23:19 +0200

    Tool to calculate basic BMI

--- src/lib/katexUtility.js
+++ src/lib/katexUtility.js
@@ -1,3 +1,19 @@
+import * as katex from "katex";
+import "katex/dist/katex.min.css";
+
+export function katexAsHtml(input) {
+    return katex.renderToString(input, {
+        displayMode: true,
+        throwOnError: false,
+    });
+}
+
+export function katexAsHtmlInline(input) {
+    return katex.renderToString(input, {
+        throwOnError: false,
+    });
+}
+
 export function toKatexVector(vec) {
     const arr = vec.toArray();
     return `\\begin{bmatrix}${arr.map(x => `${x}`).join('\\\\')}\\end{bmatrix}`;
---
+++ src/routes/tool/bmi/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { katexAsHtml } from "$lib/katexUtility.js";
+
+    let massKg = 80;
+    let heightCm = 180;
+    $: heightM = heightCm/100;
+    $: bmiBasic = massKg/(heightM**2);
+    $: bmiCalculationKatex = katexAsHtml(`\\text{BMI} = \\frac{mass_{kg}}{height_{m}^2} = \\frac{${massKg}}{${heightM}^2} = ${bmiBasic.toFixed(2)} \\frac{kg}{m^2}`)
+</script>
+
+<svelte:head>
+    <title>Body Mass Index (BMI)</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Body Mass Index (BMI)</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        {@html bmiCalculationKatex}
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Body_mass_index" target="_blank">Body mass index</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
