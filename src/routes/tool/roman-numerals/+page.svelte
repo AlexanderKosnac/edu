c27fac2c4716bae0fbdd4ad5c92015fa3b7a0dcd
Alexander Kosnac <alexander.kosnac@web.de>
2025-05-03 23:07:02 +0200

    Frame for Roman Numerals tool

---
+++ src/routes/tool/roman-numerals/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Roman Numerals</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Roman Numerals</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <table class="table table-bordered w-auto mt-1">
+            <tbody>
+                <tr>
+                    <td>I</td>
+                    <td>V</td>
+                    <td>X</td>
+                    <td>L</td>
+                    <td>C</td>
+                    <td>D</td>
+                    <td>M</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>5</td>
+                    <td>10</td>
+                    <td>50</td>
+                    <td>100</td>
+                    <td>500</td>
+                    <td>1000</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Roman_numerals" target="_blank">Roman numerals</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
