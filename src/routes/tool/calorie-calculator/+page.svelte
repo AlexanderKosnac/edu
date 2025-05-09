e547d40674dec23e3e3560376126901e081271c6
Alexander Kosnac <alexander.kosnac@web.de>
2025-05-09 22:46:44 +0200

    Frame for calorie calculator

---
+++ src/routes/tool/calorie-calculator/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { katexAsHtml } from "$lib/katexUtility.js";
+
+</script>
+
+<svelte:head>
+    <title>Calorie Calculator</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Calorie Calculator</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">            
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Calorie" target="_blank">Calorie</a></li>
+            <li><a href="https://en.wikipedia.org/wiki/Basal_metabolic_rate" target="_blank">Basal metabolic rate</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
