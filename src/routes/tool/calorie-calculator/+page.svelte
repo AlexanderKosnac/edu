739044cfcc201b3f807f32484ea5bb0425a5032c
Alexander Kosnac <alexander.kosnac@web.de>
2025-05-15 22:22:50 +0200

    Restructure calorie calculator UI

--- src/routes/tool/calorie-calculator/+page.svelte
+++ src/routes/tool/calorie-calculator/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { katexAsHtml } from "$lib/katexUtility.js";
+    import { katexAsHtml, katexAsHtmlInline } from "$lib/katexUtility.js";
 
-    let massKg = 80; // W
-    let heightCm = 180; // H
-    let ageYears = 25; // A
-    let bodyFatPercentageDecimal = 20; // F
-    $: bodyFatPercentageFractional = bodyFatPercentageDecimal / 100; // F
+    let massKg = 80;
+    let heightCm = 180;
+    let ageYears = 25;
+    let bodyFatPercentageDecimal = 20;
+    $: bodyFatPercentageFractional = bodyFatPercentageDecimal / 100;
 
     $: mifflingResultMen = 10 * massKg + 6.25 * heightCm - 5 * ageYears + 5;
     $: mifflingResultWomen = 10 * massKg + 6.25 * heightCm - 5 * ageYears - 161;
@@ -50,45 +50,28 @@
         </div>
 
         <div>
-            <h2>Base Metabolic Rate (BMR)</h2>
+            <h2>Basal Metabolic Rate (BMR)</h2>
+
+            The rate of energy expenditure per unit of time by a human at rest, typically measured in {@html katexAsHtmlInline(kcalPerDay)}.
 
             <table class="table table-bordered w-auto mt-1">
                 <tbody>
                     <tr>
-                        <th colspan="3">Mifflin-St Jeor Equation:</th>
+                        <th></th>
+                        <th>Mifflin-St Jeor</th>
+                        <th>Revised Harris-Benedict</th>
+                        <th>Katch-McArdle</th>
                     </tr>
                     <tr>
                         <td class="align-middle">Men</td>
-                        <td>{@html katexAsHtml("10 * W + 6.25 * H - 5 * A + 5")}</td>
-                        <td>{@html katexAsHtml(`${mifflingResultMen.toFixed(3)} ${kcalPerDay}`)}</td>
+                        <td>{@html katexAsHtml(`${mifflingResultMen.toFixed(3)}`)}</td>
+                        <td>{@html katexAsHtml(`${harrisResultMen.toFixed(3)}`)}</td>
+                        <td class="align-middle" rowspan="2">{@html katexAsHtml(`${katchResult.toFixed(3)}`)}</td>
                     </tr>
                     <tr>
                         <td class="align-middle">Women</td>
-                        <td>{@html katexAsHtml("10 * W + 6.25 * H - 5 * A - 161")}</td>
-                        <td>{@html katexAsHtml(`${mifflingResultWomen.toFixed(3)} ${kcalPerDay}`)}</td>
-                    </tr>
-
-                    <tr>
-                        <th colspan="3">Revised Harris-Benedict Equation:</th>
-                    </tr>
-                    <tr>
-                        <td class="align-middle">Men</td>
-                        <td>{@html katexAsHtml("13.397 * W + 4.799 * H - 5.677 * A + 88.362")}</td>
-                        <td>{@html katexAsHtml(`${harrisResultMen.toFixed(3)} ${kcalPerDay}`)}</td>
-                    </tr>
-                    <tr>
-                        <td class="align-middle">Women</td>
-                        <td>{@html katexAsHtml("9.247 * W + 3.098 * H - 4.330 * A + 447.593")}</td>
-                        <td>{@html katexAsHtml(`${harrisResultWomen.toFixed(3)} ${kcalPerDay}`)}</td>
-                    </tr>
-
-                    <tr>
-                        <th colspan="3">Katch-McArdle Formula:</th>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>{@html katexAsHtml("370 + 21.6 * (1 - F) * W")}</td>
-                        <td>{@html katexAsHtml(`${katchResult.toFixed(3)} ${kcalPerDay}`)}</td>
+                        <td>{@html katexAsHtml(`${mifflingResultWomen.toFixed(3)}`)}</td>
+                        <td>{@html katexAsHtml(`${harrisResultWomen.toFixed(3)}`)}</td>
                     </tr>
                 </tbody>
             </table>
