16377bf555df44e22434f5e02b7e10e4340b19d2
Alexander Kosnac <alexander.kosnac@web.de>
2025-06-06 23:37:23 +0200

    Basic number generator for randomizer

--- src/routes/tool/randomizer/+page.svelte
+++ src/routes/tool/randomizer/+page.svelte
@@ -1,4 +1,24 @@
 <script>
+    let min = -5;
+    let minInclusive;
+    let max = 10;
+    let maxInclusive;
+    let asInteger;
+
+    let randomNumber;
+
+    function generateRandomNumber() {
+        randomNumber = getRandomNumber(min, max, minInclusive, maxInclusive, asInteger);
+    }
+
+    function getRandomNumber(min, max, inclusiveMin, inclusiveMax, asInteger) {
+        let lower = inclusiveMin ? min : min + Number.EPSILON;
+        let upper = inclusiveMax ? max + Number.EPSILON : max;
+
+        let rand = Math.random() * (upper - lower) + lower;
+
+        return asInteger ? Math.floor(rand) : rand;
+    }
 </script>
 
 <svelte:head>
@@ -12,8 +32,35 @@
 </div>
 
 <div class="row">
-    <div class="col">
+    <div class="col-auto">
+        <div class="d-flex flex-column gap-1 align-items-start">
+            <label>
+                Minimum:
+                <input type="number" class="form-control" bind:value={min}/>
+            </label>
+            <label>
+                <input type="checkbox" class="form-check-input" bind:checked={minInclusive}/>
+                Inclusive
+            </label>
 
+            <label>
+                Maximum:
+                <input type="number" class="form-control" bind:value={max}/>
+            </label>
+            <label>
+                <input type="checkbox" class="form-check-input" bind:checked={maxInclusive}/>
+                Inclusive
+            </label>
+
+            <label>
+                <input type="checkbox" class="form-check-input" bind:checked={asInteger}/>
+                Integer Result
+            </label>
+            <button type="button" class="btn btn-primary" on:click={generateRandomNumber}>Generate</button>
+        </div>
+    </div>
+    <div class="col">
+        <span class="fs-1 align-self-center">{randomNumber}</span>
     </div>
 </div>
 
