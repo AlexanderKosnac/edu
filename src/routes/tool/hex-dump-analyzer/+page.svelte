9bc6519f952643b763ebd98aefdcd4d3ef436a91
Alexander Kosnac <alexander.kosnac@web.de>
2025-07-28 22:35:58 +0200

    Frame for hex dump analyzer

---
+++ src/routes/tool/hex-dump-analyzer/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Hex Dump Analyzer</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Hex Dump Analyzer</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Hex_dump" target="_blank">Hex dump</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
