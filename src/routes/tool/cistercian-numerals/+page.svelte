b8c2e3edf7b08923a80466e64466ea4c5b8e4fba
Alexander Kosnac <alexander.kosnac@web.de>
2025-07-30 22:59:17 +0200

    Frame for Cistercian numerals

---
+++ src/routes/tool/cistercian-numerals/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Cistercian numerals</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Cistercian numerals</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Cistercian_numerals" target="_blank">Cistercian numerals</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
