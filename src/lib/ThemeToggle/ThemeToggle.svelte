f6dd698515abd35b308a644d3866e66eb7b2f986
Alexander Kosnac <alexander.kosnac@web.de>
2025-08-01 23:25:41 +0200

    Slight animation for theme toggle

--- src/lib/ThemeToggle/ThemeToggle.svelte
+++ src/lib/ThemeToggle/ThemeToggle.svelte
@@ -28,22 +28,25 @@
     <!-- svelte-ignore a11y_no_static_element_interactions -->
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
         fill="currentColor" on:click={toggleTheme} aria-label="Toggle theme">
-        {#if theme === light}
-            <g id="moon">
-                <path d="M21 12a9 9 0 1 1-9-9c0 0 0 0 0 0a7 7 0 1 0 9 9z"/>
+        <g id="moon" class="theme-toggle" class:invisible={theme === dark}>
+            <path d="M21 12a9 9 0 1 1-9-9c0 0 0 0 0 0a7 7 0 1 0 9 9z"/>
+        </g>
+        <g id="sun" class="theme-toggle" class:invisible={theme === light}>
+            <circle cx="12" cy="12" r="5"/>
+            <g stroke="currentColor" stroke-width="2">
+                {#each { length: 8 } as _, i}
+                    <line x1="12" y1="1" x2="12" y2="5" transform="rotate({i * 45}, 12, 12)"/>
+                {/each}
             </g>
-        {:else}
-            <g id="sun">
-                <circle cx="12" cy="12" r="5"/>
-                <g stroke="currentColor" stroke-width="2">
-                    {#each { length: 8 } as _, i}
-                        <line x1="12" y1="1" x2="12" y2="5" transform="rotate({i * 45}, 12, 12)"/>
-                    {/each}
-                </g>
-            </g>
-        {/if}
+        </g>
     </svg>
 {/if}
 
 <style>
+    .theme-toggle {
+        transition: opacity 0.5s ease;
+    }
+    .invisible {
+        opacity: 0;
+    }
 </style>
