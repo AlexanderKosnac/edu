c29007a8b13cf42daefd1ab6bbf62afe869ababb
Alexander Kosnac <alexander.kosnac@web.de>
2025-08-19 23:21:38 +0200

    Frame for Pi tool

---
+++ src/routes/tool/pi/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Pi</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Pi</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Pi" target="_blank">Pi</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
