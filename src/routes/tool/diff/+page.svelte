246eb38389757781e9ba9c9b0659f68cc7684a8b
Alexander Kosnac <alexander.kosnac@web.de>
2025-08-26 22:49:21 +0200

    Basic inputs for diff tool

--- src/routes/tool/diff/+page.svelte
+++ src/routes/tool/diff/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    let text1 = "";
+    let text2 = "";
 </script>
 
 <svelte:head>
@@ -13,6 +15,13 @@
 
 <div class="row">
     <div class="col">
+        <label for="inputText">Text 1:</label>
+        <textarea class="form-control font-monospace" rows="20" bind:value="{text1}"></textarea>
+    </div>
+
+    <div class="col">
+        <label for="outputText">Text 2:</label>
+        <textarea class="form-control font-monospace" rows="20" bind:value="{text2}"></textarea>
     </div>
 </div>
 
