6ce90a408ce177c44c9ac232ecd79f615ca61968
Alexander Kosnac <alexander.kosnac@web.de>
2025-08-30 20:55:34 +0200

    Fix implementation of Jaro distance

--- src/lib/editDistance.js
+++ src/lib/editDistance.js
@@ -1,33 +1,44 @@
 export function jaroDistance(s1, s2) {
-    if (!s1.length && !s2.length)
-        return 1;
-    if (!s1.length || !s2.length)
-        return 0;
+    if (s1 === s2)
+        return 1.0;
 
     const [len1, len2] = [s1.length, s2.length];
-    const win = Math.floor(Math.max(len1, len2) / 2) - 1;
-    const m1 = [], m2 = [];
-    const s2used = Array(len2).fill(false);
+    if (len1 === 0 || len2 === 0)
+        return 0.0;
 
+    const window = Math.floor(Math.max(len1, len2) / 2) - 1;
+
+    let s1Matches = new Array(len1).fill(false);
+    let s2Matches = new Array(len2).fill(false);
+
+    let m = 0;
     for (let i = 0; i < len1; i++) {
-        const wstart = Math.max(0, i - win);
-        const wend = Math.min(len2, i + win + 1);
-        for (let j = wstart; j < wend; j++) {
-            if (!s2used[j] && s1[i] === s2[j]) {
-                m1.push(s1[i]);
-                m2.push(s2[j]);
-                s2used[j] = true;
-                break;
-            }
+        const start = Math.max(0, i - window);
+        const end = Math.min(i + window + 1, len2);
+
+        for (let j = start; j < end; j++) {
+            if (s2Matches[j] || s1[i] !== s2[j])
+                continue;
+            s1Matches[i] = true;
+            s2Matches[j] = true;
+            m++;
+            break;
         }
     }
-    const m = m1.length;
+
     if (m === 0)
-        return 0;
+        return 0.0;
 
     let t = 0;
-    for (let k = 0; k < m; k++) if (m1[k] !== m2[k]) t++;
-    t = Math.floor(t / 2);
+    let k = 0;
+    for (let i = 0; i < len1; i++) {
+        if (!s1Matches[i]) continue;
+        while (!s2Matches[k]) k++;
+        if (s1[i] !== s2[k]) t++;
+        k++;
+    }
 
-    return (m / len1 + m / len2 + (m - t) / m) / 3;
+    t = t / 2;
+
+    return (m / len1 + m / len2 + (m - t) / m) / 3.0;
 }
