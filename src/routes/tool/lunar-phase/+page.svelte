b16316c59bb91b3eabe776bf8746988c0706a009
Alexander Kosnac <alexander.kosnac@web.de>
2025-09-07 19:49:41 +0200

    Frame for Lunar phase tool

---
+++ src/routes/tool/lunar-phase/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Lunar Phase</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Lunar Phase</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="en.wikipedia.org/wiki/Lunar_phase" target="_blank">Lunar phase</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
