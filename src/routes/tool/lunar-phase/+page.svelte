a016d39e37018bff173d0d7b0af3b41908a3c5a4
Alexander Kosnac <alexander.kosnac@web.de>
2025-09-12 23:14:05 +0200

    Basic implementation for lunar phase

--- src/routes/tool/lunar-phase/+page.svelte
+++ src/routes/tool/lunar-phase/+page.svelte
@@ -1,4 +1,53 @@
 <script>
+    import { onMount } from "svelte";
+
+    let selectedDateString = getDateLocal();
+    $: selectedDate = new Date(selectedDateString);
+
+    let phaseName;
+
+    function updateLunarPhase() {
+        phaseName = getMoonPhaseName(selectedDate);
+    }
+
+    function getDateLocal(date = new Date()) {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    function getMoonPhase(date = new Date()) {
+        const newMoonRef = new Date(Date.UTC(2000, 0, 6, 18, 14)); // Reference New Moon date: 2000-01-06 18:14 UTC
+        const synodicMonth = 29.530588853; // average lunar month length (days)
+
+        const diff = (date.getTime() - newMoonRef.getTime()) / 1000 / 86400;
+        const lunations = diff / synodicMonth;
+
+        const phaseIndex = Math.floor((lunations - Math.floor(lunations)) * 8);
+
+        return phaseIndex;
+    }
+
+    function getMoonPhaseName(date = new Date()) {
+        const phases = [
+            "New Moon",
+            "Waxing Crescent",
+            "First Quarter",
+            "Waxing Gibbous",
+            "Full Moon",
+            "Waning Gibbous",
+            "Last Quarter",
+            "Waning Crescent"
+        ];
+
+        return phases[getMoonPhase(date)];
+    }
+
+    onMount(() => {
+        updateLunarPhase();
+    });
 </script>
 
 <svelte:head>
@@ -12,7 +61,14 @@
 </div>
 
 <div class="row">
+    <div class="col-auto">
+        <label>
+            Date:
+            <input type="date" class="form-control" bind:value={selectedDateString} on:input={updateLunarPhase}/>
+        </label>
+    </div>
     <div class="col">
+        {phaseName}
     </div>
 </div>
 
