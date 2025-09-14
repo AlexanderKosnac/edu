2dd984415a69c7eb4356bf0c7002b42b36545850
Alexander Kosnac <alexander.kosnac@web.de>
2025-09-14 18:10:36 +0200

    Frame for CPU simulator

---
+++ src/routes/tool/cpu-simulator/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>CPU simulator</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>CPU simulator</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://www.mathematik.uni-marburg.de/~thormae/lectures/ti1/code/cpusim/index.html" target="_blank">Simulator by Marburg University</a></li>
+            <li><a href="https://www.uni-marburg.de/de/fb12/arbeitsgruppen/grafikmultimedia/lehre/ti" target="_blank">TI lecture by Marburg University</a></li>
+            <li><a href="https://www.mathematik.uni-marburg.de/~thormae/lectures/ti1/ti_9_1_ger_web.html" target="_blank">Slides related to the CPU simulator</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
