aba6cb7b4572a2be05623b8ad8d196b543c365e2
Alexander Kosnac <alexander.kosnac@web.de>
2025-10-03 21:29:10 +0200

    Basic loading of Exif data from image

--- src/routes/tool/exif-viewer/+page.svelte
+++ src/routes/tool/exif-viewer/+page.svelte
@@ -1,4 +1,19 @@
 <script>
+    import ExifReader from "exifreader";
+
+    let imageInput;
+
+    let tags;
+
+    async function loadExif() {
+        const file = imageInput.files[0];
+        const buffer = await file.arrayBuffer();
+
+        tags = ExifReader.load(buffer, {
+            async: true,
+            expanded: true,
+        });
+    }
 </script>
 
 <svelte:head>
@@ -13,6 +28,11 @@
 
 <div class="row">
     <div class="col-auto">
+        <input bind:this={imageInput} type="file" class="form-control" id="img" name="img" accept="image/*" onchange={loadExif}>
+        <button type="button" class="btn btn-primary" onclick={loadExif}>Load Exif</button>
+    </div>
+    <div class="col">
+        <pre>{JSON.stringify(tags, null, 2)}</pre>
     </div>
 </div>
 
@@ -20,6 +40,7 @@
     <div class="col">
         <h2>References:</h2>
         <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Exif" target="_blank">Exif</a></li>
             <li><a href="https://www.npmjs.com/package/exifreader/v/3.2.0" target="_blank">exifreader npm package</a></li>
         </ul>
     </div>
