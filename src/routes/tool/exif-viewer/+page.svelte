a5d33673414fc4fbc15fe36478b814637c48a040
Alexander Kosnac <alexander.kosnac@web.de>
2025-10-03 20:24:46 +0200

    Frame for Exif viewer

---
+++ src/routes/tool/exif-viewer/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Exif Viewer</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Exif Viewer</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-auto">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://www.npmjs.com/package/exifreader/v/3.2.0" target="_blank">exifreader npm package</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
