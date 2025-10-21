6974b83cf0faf62f6770635d428919e5b918ca47
Alexander Kosnac <alexander.kosnac@web.de>
2025-10-21 19:28:30 +0200

    Frame for SVG path builder

---
+++ src/routes/tool/svg-path-builder/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>SVG path builder</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>SVG path builder</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorials/SVG_from_scratch/Paths" target="_blank">MDN SVG Path documentation</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
