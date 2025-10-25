0b8aa0a0d0372b52dfe6a059fbd88b78930fb137
Alexander Kosnac <alexander.kosnac@web.de>
2025-10-25 17:10:39 +0200

    Frame for Ceasar cipher

---
+++ src/routes/tool/ceasar-cipher/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Caesar cipher</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Caesar cipher</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Caesar_cipher" target="_blank">Caesar cipher</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
