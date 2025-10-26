675f5fb6d7662793c7dfca05ec2b30ddcbeb9452
Alexander Kosnac <alexander.kosnac@web.de>
2025-10-26 19:16:41 +0100

    Implementation of Ceasar cipher

--- src/routes/tool/ceasar-cipher/+page.svelte
+++ src/routes/tool/ceasar-cipher/+page.svelte
@@ -1,15 +1,36 @@
 <script>
     let cypherShift = 23;
 
+    let plaintext = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    let ciphertext = "";
+
+    function charIsIgnored(c) {
+        const n = c.charCodeAt(0);
+        const isLetter = (n >= 0x41 && n <= 0x5A) || (n >= 0x61 && n <= 0x7A);
+        return !isLetter;
+    }
+
     function letterAsInteger(l) {
-        return l.charCodeAt(0) - 0x41;
+        return l.charCodeAt(0) + 0x41;
     }
 
     function integerAsLetter(i) {
         return String.fromCharCode((i % 26) + 0x41);
     }
 
+    function onPlaintextChanged() {
+        ciphertext = "";
+        for (const c of plaintext.toUpperCase()) {
+            ciphertext += charIsIgnored(c) ? c : integerAsLetter(letterAsInteger(c) + cypherShift);
+        }
+    }
 
+    function onCiphertextChanged() {
+        plaintext = "";
+        for (const c of ciphertext.toUpperCase()) {
+            plaintext += charIsIgnored(c) ? c : integerAsLetter(letterAsInteger(c) - cypherShift);
+        }
+    }
 </script>
 
 <svelte:head>
@@ -33,7 +54,7 @@
                 <tr>
                     <th></th>
                     {#each { length: 26 } as _, i}
-                        <td>{i + 1}</td>
+                        <th>{i + 1}</th>
                     {/each}
                 </tr>
                 <tr>
@@ -50,7 +71,20 @@
                 </tr>
             </tbody>
         </table>
+    </div>
+</div>
 
+<div class="row">
+    <div class="col">
+        <div class="form-group">
+            <label for="inputText">Plaintext:</label>
+            <textarea class="form-control font-monospace" id="inputText" pattern="[A-Za-z0-9]+" rows="20" bind:value="{plaintext}" oninput={onPlaintextChanged}></textarea>
+        </div>
+    </div>
+
+    <div class="col">
+        <label for="outputText">Ciphertext:</label>
+        <textarea class="form-control font-monospace" id="outputText" rows="20" bind:value="{ciphertext}" oninput={onCiphertextChanged}></textarea>
     </div>
 </div>
 
