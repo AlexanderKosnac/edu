453e605b626534c4c3e163ec472a9c2c21a37a9e
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-01 22:12:59 +0100

    Extract color contrasting function into utility

--- src/routes/tool/hex-dump-analyzer/+page.svelte
+++ src/routes/tool/hex-dump-analyzer/+page.svelte
@@ -2,6 +2,7 @@
     import { hexStringToByteArray } from "$lib/hexUtility";
     import { hexHttpRequest } from "./data.js";
     import { toHex } from "$lib/hexUtility";
+    import { getContrastingColor } from "$lib/colorUtility";
 
     let hexInput = hexHttpRequest;
     let startAddress = 0x34;
@@ -53,18 +54,6 @@
     function removeHighlight(i) {
         highlights.splice(i, 1);
         highlights = highlights;
-    }
-
-    function getContrastingColor(bgColor) {
-        let r = parseInt(bgColor.substr(1, 2), 16) / 255;
-        let g = parseInt(bgColor.substr(3, 2), 16) / 255;
-        let b = parseInt(bgColor.substr(5, 2), 16) / 255;
-
-        [r, g, b] = [r, g, b].map(c => c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4));
-
-        let lb = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-
-        return (lb > 0.179) ? "#000000" : "#FFFFFF";
     }
 
     $: sanitizedHexInput = hexInput.replace(/[^0-9a-fA-F]/g, '');
