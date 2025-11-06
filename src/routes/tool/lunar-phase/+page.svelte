f737bb19f28c49c1b66e44e6f7477e2e292cedd7
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-06 19:37:05 +0100

    Clean up lunar phase UI

--- src/routes/tool/lunar-phase/+page.svelte
+++ src/routes/tool/lunar-phase/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     let selectedDatetimeString = getDateLocal();
 
-    const newMoonRef = new Date(Date.UTC(2024, 0, 11, 11, 57));
+    const newMoonRef = new Date(Date.UTC(2000, 0, 6, 18, 14, 0)); // Meeus Lunation Number 0
     const synodicMonth = 29.53058867; // average lunar month length (days)
 
     const phases = [
@@ -107,8 +107,11 @@
     {@const moonRadius = radius * 0.2}
     {@const earthRadius = radius * 0.4}
     <svg width={size} height={size} viewBox="-{Math.floor(size/2)} -{Math.floor(size/2)} {size} {size}" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="0" cy="0" r="{earthRadius}" fill="blue" />
+        <path d="M 0,-{earthRadius} A 1,1 0 0 0 0,{earthRadius} Z" fill="#0000aa" />
+        <path d="M 0,-{earthRadius} A 1,1 0 0 1 0,{earthRadius} Z" fill="#3333ff" />
+
         <circle cx="0" cy="0" r={radius} fill="none" stroke="currentColor" stroke-dasharray="4" />
+
         <g transform="rotate({-phaseFractionRotation}) translate({radius}, 0) rotate({phaseFractionRotation})">
             <path d="M 0,-{moonRadius} A 1,1 0 0 0 0,{moonRadius} Z" fill="#444" />
             <path d="M 0,-{moonRadius} A 1,1 0 0 1 0,{moonRadius} Z" fill="#f4f4f4" />
@@ -120,10 +123,14 @@
 {/snippet}
 
 <div class="row">
-    <div class="col-auto">
+    <div class="col-auto d-flex flex-column gap-1">
+        <div>
+            New moon reference date: {newMoonRef}<br>
+            Average lunar month length (days) {synodicMonth} days
+        </div>
         <div class="input-group">
             <span class="input-group-text">Date:</span>
-            <input type="datetime-local" class="form-control" bind:value={selectedDatetimeString} />
+            <input type="datetime-local" class="form-control" bind:value={selectedDatetimeString} style="max-width: 20em" />
             <button type="button" class="btn btn-secondary" onclick={() => changeTime(-1)}>-1</button>
             <button type="button" class="btn btn-secondary" onclick={() => changeTime(+1)}>+1</button>
             <select class="form-select" id="stepSelect" bind:value={currentTimeUnit} style="max-width: 8em">
@@ -132,26 +139,18 @@
                 <option value="minute">Minute</option>
             </select>
         </div>
-        New moon reference date: {newMoonRef}<br>
-        Average lunar month length (days) {synodicMonth} days
-    </div>
-    <div class="col">
-        <div>
-            <input type="range" class="form-range" min="0" max="1" step="0.01" bind:value={currentPhaseFraction}>
-            <span>pf: {currentPhaseFraction};</span>
-        </div>
-    </div>
-</div>
 
-<div class="row">
-    <div class="col">
-        <div class="d-flex gap-1">
-            <div class="d-flex flex-column align-items-center">
-                {@render moon(300, 120, currentPhaseFraction)}
-                {currentPhaseName}
+        <div class="row">
+            <div class="col">
+                <div class="d-flex flex-column align-items-center">
+                    {@render moon(400, 160, currentPhaseFraction)}
+                    {currentPhaseName}
+                </div>
             </div>
-            <div>
-                {@render moonEarthRelation(300, 120, currentPhaseFraction)}
+            <div class="col">
+                <div class="d-flex flex-column align-items-center">
+                    {@render moonEarthRelation(400, 160, currentPhaseFraction)}
+                </div>
             </div>
         </div>
     </div>
