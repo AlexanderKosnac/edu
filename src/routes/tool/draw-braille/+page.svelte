e74568909ffa5445b06bc9383afc58d16a318210
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-10 19:14:08 +0100

    Frame for braille drawing tool

---
+++ src/routes/tool/draw-braille/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Draw braille</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Draw braille</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Braille_Patterns" target="_blank">Braille Patterns</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
