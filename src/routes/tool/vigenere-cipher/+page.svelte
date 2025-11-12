8128c1af0b968cfb288911966ce4bd3781006fe1
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-12 19:59:34 +0100

    Frame for Vigenere cipher

---
+++ src/routes/tool/vigenere-cipher/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Vigenere cipher</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Vigenere cipher</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher" target="_blank">Vigenere cipher</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
