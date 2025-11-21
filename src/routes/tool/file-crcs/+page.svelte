07feb0509c8a163d92b9770f14ff8508ecfd6d1c
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-21 22:09:31 +0100

    Frame for mass CRC tool

---
+++ src/routes/tool/file-crcs/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>File CRCs</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>File CRCs</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Checksum" target="_blank">Checksum</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
