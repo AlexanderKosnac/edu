b2487722bdbf151f4fa8656c9c57d5d98d2f8c7c
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-22 19:34:27 +0100

    Implement more CRCs for file CRCs tool

--- src/routes/tool/file-crcs/+page.svelte
+++ src/routes/tool/file-crcs/+page.svelte
@@ -1,5 +1,25 @@
 <script>
-    import { md5 } from "hash-wasm";
+/* To be implemented:
+blake2b(data: IDataType, bits?: number, key?: IDataType): Promise<string> // default is 512 bits
+blake2s(data: IDataType, bits?: number, key?: IDataType): Promise<string> // default is 256 bits
+blake3(data: IDataType, bits?: number, key?: IDataType): Promise<string> // default is 256 bits
+crc32(data: IDataType, polynomial?: number): Promise<string> // default polynomial is 0xedb88320, for CRC32C use 0x82f63b78
+crc64(data: IDataType, polynomial?: string): Promise<string> // default polynomial is 'c96c5795d7870f42' (ECMA)
+keccak(data: IDataType, bits?: 224 | 256 | 384 | 512): Promise<string> // default is 512 bits
+sha3(data: IDataType, bits?: 224 | 256 | 384 | 512): Promise<string> // default is 512 bits
+xxhash32(data: IDataType, seed?: number): Promise<string>
+xxhash64(data: IDataType, seedLow?: number, seedHigh?: number): Promise<string>
+xxhash3(data: IDataType, seedLow?: number, seedHigh?: number): Promise<string>
+xxhash128(data: IDataType, seedLow?: number, seedHigh?: number): Promise<string>
+*/
+    import {
+        adler32,
+        md4, md5,
+        ripemd160,
+        sha1, sha224, sha256, sha384, sha512,
+        sm3,
+        whirlpool,
+    } from "hash-wasm";
 
     let results = {};
 
@@ -10,13 +30,22 @@
         if (!files.length)
             return;
 
-
         for (let file of files) {
             const buffer = await file.arrayBuffer();
             const bytes = new Uint8Array(buffer);
 
             results[file.name] = {
+                adler32: await adler32(bytes),
+                md4: await md4(bytes),
                 md5: await md5(bytes),
+                ripemd160: await ripemd160(bytes),
+                sha1: await sha1(bytes),
+                sha224: await sha224(bytes),
+                sha256: await sha256(bytes),
+                sha384: await sha384(bytes),
+                sha512: await sha512(bytes),
+                sm3: await sm3(bytes),
+                whirlpool: await whirlpool(bytes),
             };
         }
     }
@@ -37,9 +66,9 @@
         <input type="file" class="form-control" id="img" name="img" accept="*/*" multiple onchange={handleFile}>
 
         {#if Object.keys(results).length > 0}
-            {#each Object.keys(results) as [file, crcs]}
+            {#each Object.entries(results) as [file, crcs]}
                 <h2>Results for: {file}</h2>
-                {#each Object.keys(crcs) as [name, value]}
+                {#each Object.entries(crcs) as [name, value]}
                     {name}: {value}<br>
                 {/each}
             {/each}
@@ -52,6 +81,7 @@
         <h2>References:</h2>
         <ul>
             <li><a href="https://en.wikipedia.org/wiki/Checksum" target="_blank">Checksum</a></li>
+            <li><a href="https://www.npmjs.com/package/hash-wasm" target="_blank">hash-wasm npm package</a></li>
         </ul>
     </div>
 </div>
