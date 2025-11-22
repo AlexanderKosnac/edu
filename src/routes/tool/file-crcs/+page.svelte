f5b2d5007c677c71ffcc68aea259d40b2751f63b
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-22 19:18:44 +0100

    MD5 for file CRC tool

--- src/routes/tool/file-crcs/+page.svelte
+++ src/routes/tool/file-crcs/+page.svelte
@@ -1,4 +1,25 @@
 <script>
+    import { md5 } from "hash-wasm";
+
+    let results = {};
+
+    async function handleFile(event) {
+        results = {};
+
+        const files = event.target.files;
+        if (!files.length)
+            return;
+
+
+        for (let file of files) {
+            const buffer = await file.arrayBuffer();
+            const bytes = new Uint8Array(buffer);
+
+            results[file.name] = {
+                md5: await md5(bytes),
+            };
+        }
+    }
 </script>
 
 <svelte:head>
@@ -13,6 +34,16 @@
 
 <div class="row">
     <div class="col">
+        <input type="file" class="form-control" id="img" name="img" accept="*/*" multiple onchange={handleFile}>
+
+        {#if Object.keys(results).length > 0}
+            {#each Object.keys(results) as [file, crcs]}
+                <h2>Results for: {file}</h2>
+                {#each Object.keys(crcs) as [name, value]}
+                    {name}: {value}<br>
+                {/each}
+            {/each}
+        {/if}
     </div>
 </div>
 
