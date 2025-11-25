93df0361bf5fe02b0cc1a4f8343c0c029c1c8f41
Alexander Kosnac <alexander.kosnac@web.de>
2025-11-25 20:47:54 +0100

    Frame for Cholesky decomposition

---
+++ src/routes/tool/cholesky-decomposition/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Cholseky decomposition</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Cholseky decomposition</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Cholesky_decomposition" target="_blank">Cholseky decomposition</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
