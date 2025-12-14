3dffc48c828841d3eb76eca9ebab48e71dd02a54
Alexander Kosnac <alexander.kosnac@web.de>
2025-12-14 19:33:46 +0100

    Frame for Bundestag composition

---
+++ src/routes/tool/bundestag-composition/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Composition of the Bundestag</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Composition of the Bundestag</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://www.bundestag.de/services/opendata" target="_blank">Open Data Service by the Bundestag</a></li>
+            <li><a href="https://dip.bundestag.de/%C3%BCber-dip/hilfe/api" target="_blank">Bundestag API</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
