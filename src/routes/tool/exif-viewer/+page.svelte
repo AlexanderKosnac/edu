fbe70d0048ef00e9cfbcfdef61ffe9ab5e58924f
Alexander Kosnac <alexander.kosnac@web.de>
2025-12-25 19:07:29 +0100

    Move exif viewer sections into tabs

--- src/routes/tool/exif-viewer/+page.svelte
+++ src/routes/tool/exif-viewer/+page.svelte
@@ -64,94 +64,101 @@
 </div>
 
 {#if tags}
-<div class="row">
+<div class="row mb-1">
     <div class="col">
         <button type="button" class="btn btn-primary" onclick={() => openJson(tags)}>View JSON</button>
     </div>
 </div>
 
 <div class="row">
-    <h2>Thumbnail</h2>
-    {#if tags.Thumbnail}
-        <div class="col">
-            <table class="table table-bordered w-auto mt-1">
-                <tbody>
-                    <tr>
-                        <td>Thumbnail</td>
-                        <td><img src="data:{tags.Thumbnail.type};base64, {tags.Thumbnail.base64}" alt="Thumbnail" /></td>
-                    </tr>
-                    <tr>
-                        <td>Base64</td>
-                        <td><input type="text" class="form-control" value={tags.Thumbnail.base64} readonly/></td>
-                    </tr>
-                    <tr>
-                        <td>Type</td>
-                        <td><samp>{tags.Thumbnail.type}</samp></td>
-                    </tr>
-                </tbody>
-            </table>
-            {@render descriptionTable(tags.Thumbnail)}
+    <div class="col">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="thumbnail-tab" data-bs-toggle="tab" data-bs-target="#thumbnail" type="button" role="tab" aria-controls="thumbnail" aria-selected="true">Thumbnail</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="file-tab" data-bs-toggle="tab" data-bs-target="#file" type="button" role="tab" aria-controls="file" aria-selected="false">File</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="exif-tab" data-bs-toggle="tab" data-bs-target="#exif" type="button" role="tab" aria-controls="exif" aria-selected="false">Exif</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="icc-tab" data-bs-toggle="tab" data-bs-target="#icc" type="button" role="tab" aria-controls="icc" aria-selected="false">ICC</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="xmp-tab" data-bs-toggle="tab" data-bs-target="#xmp" type="button" role="tab" aria-controls="xmp" aria-selected="false">XMP</button>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="thumbnail" role="tabpanel" aria-labelledby="thumbnail-tab">
+                {#if tags.Thumbnail}
+                    <table class="table table-bordered w-auto mt-1">
+                        <tbody>
+                            <tr>
+                                <td>Thumbnail</td>
+                                <td><img src="data:{tags.Thumbnail.type};base64, {tags.Thumbnail.base64}" alt="Thumbnail" /></td>
+                            </tr>
+                            <tr>
+                                <td>Base64</td>
+                                <td><input type="text" class="form-control" value={tags.Thumbnail.base64} readonly/></td>
+                            </tr>
+                            <tr>
+                                <td>Type</td>
+                                <td><samp>{tags.Thumbnail.type}</samp></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    {@render descriptionTable(tags.Thumbnail)}
+                {:else}
+                    No thumbnail data.
+                {/if}
+            </div>
+            <div class="tab-pane fade" id="file" role="tabpanel" aria-labelledby="file-tab">
+                {#if tags.file}
+                    <div class="col">
+                        {@render descriptionTable(tags.file)}
+                    </div>
+                {:else}
+                    <div class="col">
+                        No file data.
+                    </div>
+                {/if}
+            </div>
+            <div class="tab-pane fade" id="exif" role="tabpanel" aria-labelledby="exif-tab">
+                {#if tags.exif}
+                    <div class="col">
+                        {@render descriptionTable(tags.exif)}
+                    </div>
+                {:else}
+                    <div class="col">
+                        No Exif data.
+                    </div>
+                {/if}
+            </div>
+            <div class="tab-pane fade" id="icc" role="tabpanel" aria-labelledby="icc-tab">
+                {#if tags.icc}
+                    <div class="col">
+                        {@render descriptionTable(tags.icc)}
+                    </div>
+                {:else}
+                    <div class="col">
+                        No ICC data.
+                    </div>
+                {/if}
+            </div>
+            <div class="tab-pane fade" id="xmp" role="tabpanel" aria-labelledby="xmp-tab">
+                {#if tags.xmp}
+                    <div class="col">
+                        {@render descriptionTable(tags.xmp)}
+                    </div>
+                {:else}
+                    <div class="col">
+                        No XMP data.
+                    </div>
+                {/if}
+            </div>
         </div>
-    {:else}
-        <div class="col">
-            No thumbnail data.
-        </div>
-    {/if}        
-</div>
-
-<div class="row">
-    <h2>File</h2>
-    {#if tags.file}
-        <div class="col">
-            {@render descriptionTable(tags.file)}
-        </div>
-        <div class="col">
-            <pre>{JSON.stringify(tags.file, null, 2)}</pre>
-        </div>
-    {:else}
-        <div class="col">
-            No file data.
-        </div>
-    {/if}
-</div>
-
-<div class="row">
-    <h2>Exif</h2>
-    {#if tags.exif}
-        <div class="col">
-            {@render descriptionTable(tags.exif)}
-        </div>
-    {:else}
-        <div class="col">
-            No Exif data.
-        </div>
-    {/if}
-</div>
-
-<div class="row">
-    <h2>ICC</h2>
-    {#if tags.icc}
-        <div class="col">
-            {@render descriptionTable(tags.icc)}
-        </div>
-    {:else}
-        <div class="col">
-            No ICC data.
-        </div>
-    {/if}
-</div>
-
-<div class="row">
-    <h2>XMP</h2>
-    {#if tags.xmp}
-        <div class="col">
-            {@render descriptionTable(tags.xmp)}
-        </div>
-    {:else}
-        <div class="col">
-            No XMP data.
-        </div>
-    {/if}
+    </div>
 </div>
 {/if}
 
