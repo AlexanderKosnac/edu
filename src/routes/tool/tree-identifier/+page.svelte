14f0487eb90bcf63a590352beb1d11fe78b2094f
Alexander Kosnac <alexander.kosnac@web.de>
2026-01-08 21:30:03 +0100

    Frame for tree identifier

---
+++ src/routes/tool/tree-identifier/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Tree Identifier</title>
+</svelte:head>
+
+<div class="row">
+    <div class="col">
+        <h1>Tree Identifier</h1>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col">
+        aaa
+    </div>
+    <div class="col">
+        bbb
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <h2>References:</h2>
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/..." target="_blank">...</a></li>
+        </ul>
+    </div>
+</div>
+
+<style>
+</style>
