e091ea7d222776f31459ab792cfa312b7123bcd9
Alexander Kosnac <alexander.kosnac@web.de>
2026-01-20 19:22:13 +0100

    Centralize API functions

--- src/lib/apiBundestag.js
+++ src/lib/apiBundestag.js
@@ -2,27 +2,21 @@
  * A basic wrapper for the API provided by the Bundestag:
  * https://dip.bundestag.de/%C3%BCber-dip/hilfe/api
  */
-const CORS_PROXY = "https://corsproxy.io/?";
+import { get } from "./apiRestUtility.js";
 
 const API_KEY = "OSOegLs.PR2lwJ1dwCeje9vTj7FPOt3hvpYKtwKkhw";
 const API_BASE = "https://search.dip.bundestag.de/api/v1";
 
-async function get(path, params = {}) {
-    const query = new URLSearchParams({ ...params, format: "json", apikey: API_KEY });
-    const url = `${API_BASE}/${path}?${query.toString()}`;
-
-    const response = await fetch(CORS_PROXY + encodeURIComponent(url));
-    if (!response.ok)
-        throw new Error(`HTTP error: ${response.status}`);
-
-    return await response.json();
+async function getInternal(path, params = {}) {
+    const p = { ...params, format: "json", apikey: API_KEY };
+    return await get(`${API_BASE}/${path}`, p);
 }
 
 async function* curserBasedGenerator(path, params = {}) {
     let cursor = null;
 
     while (true) {
-        const page = await get(path, cursor ? { ...params, cursor } : params);
+        const page = await getInternal(path, cursor ? { ...params, cursor } : params);
 
         yield page.documents;
 
@@ -43,9 +37,9 @@ async function flattenCursor(path, params = {}) {
 
 function createEndpoint(endpoint) {
     return {
-        get: (params = {}) => get(endpoint, params),
+        get: (params = {}) => getInternal(endpoint, params),
         getAll: (params = {}) => flattenCursor(endpoint, params),
-        getById: (id, params = {}) => get(`${endpoint}/${id}`, params),
+        getById: (id, params = {}) => getInternal(`${endpoint}/${id}`, params),
     };
 }
 
