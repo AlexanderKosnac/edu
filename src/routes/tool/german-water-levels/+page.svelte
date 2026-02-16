36edbdaa47715c6eb06d7dc1afebbc446a0c9761
Alexander Kosnac <alexander.kosnac@web.de>
2026-02-16 18:19:40 +0100

    Handle undefined value for promise in German water levels

--- src/routes/tool/german-water-levels/+page.svelte
+++ src/routes/tool/german-water-levels/+page.svelte
@@ -121,42 +121,46 @@
         {#await allStationsPromise}
             <div>Request pending</div>
         {:then value}
-            <div class="d-flex flex-row gap-5 mb-1">
-                <div>
-                    {value?.length} stations loaded
+            {#if value}
+                <div class="d-flex flex-row gap-5 mb-1">
+                    <div>
+                        {value?.length} stations loaded
+                    </div>
+                    <div class="flex-grow-1"></div>
+                    <div class="d-flex flex-row gap-2">
+                        Display:
+                        <label>
+                            <input type="radio" class="form-check-input" value="List" bind:group={selectionMethod} />
+                            List
+                        </label>
+                        <label>
+                            <input type="radio" class="form-check-input" value="Map" bind:group={selectionMethod} />
+                            Map
+                        </label>
+                    </div>
+                    <div class="flex-grow-1"></div>
+                    <button type="button" class="btn btn-link p-0 align-baseline" onclick={() => openJson(value)}>Open json data</button>
                 </div>
-                <div class="flex-grow-1"></div>
-                <div class="d-flex flex-row gap-2">
-                    Display:
-                    <label>
-                        <input type="radio" class="form-check-input" value="List" bind:group={selectionMethod} />
-                        List
-                    </label>
-                    <label>
-                        <input type="radio" class="form-check-input" value="Map" bind:group={selectionMethod} />
-                        Map
-                    </label>
-                </div>
-                <div class="flex-grow-1"></div>
-                <button type="button" class="btn btn-link p-0 align-baseline" onclick={() => openJson(value)}>Open json data</button>
-            </div>
 
-            <div hidden={selectionMethod === "List"}>
-                <svg id="map" width={svgWidth} height={svgHeight}></svg>
-            </div>
-            <div hidden={selectionMethod === "Map"}>
-                <div class="overflow-auto" style="max-height: 600px">
-                    <ul>
-                        {#each value?.sort((a, b) => a.longname.localeCompare(b.longname, undefined, { sensitivity: "base" })) as station}
-                            <li>
-                                <button type="button" class="btn btn-link p-0 align-baseline" onclick={() => loadStation(station.uuid)}>
-                                    {station.longname}
-                                </button>
-                            </li>
-                        {/each}
-                    </ul>
+                <div hidden={selectionMethod === "List"}>
+                    <svg id="map" width={svgWidth} height={svgHeight}></svg>
                 </div>
-            </div>
+                <div hidden={selectionMethod === "Map"}>
+                    <div class="overflow-auto" style="max-height: 600px">
+                        <ul>
+                            {#each value?.sort((a, b) => a.longname.localeCompare(b.longname, undefined, { sensitivity: "base" })) as station}
+                                <li>
+                                    <button type="button" class="btn btn-link p-0 align-baseline" onclick={() => loadStation(station.uuid)}>
+                                        {station.longname}
+                                    </button>
+                                </li>
+                            {/each}
+                        </ul>
+                    </div>
+                </div>
+            {:else}
+                <div>No data.</div>
+            {/if}
         {:catch error}
             <div>Something went wrong: {error.message}</div>
         {/await}
@@ -170,66 +174,70 @@
                 <h2>Station Details</h2>
                 <div>Request pending</div>
             {:then value}
-                <h2>{value.longname}</h2>
-                <ul>
-                    <li><button type="button" class="btn btn-link p-0 align-baseline" onclick={() => openJson(value)}>Open json data</button></li>
-                    {#if value?.latitude && value?.longitude}
-                        <li>Coordinate: {value.latitude}° N, {value.longitude}° E</li>
-                    {/if}
-                    <li>Water: {value.water.longname}</li>
-                </ul>
+                {#if value}
+                    <h2>{value?.longname}</h2>
+                    <ul>
+                        <li><button type="button" class="btn btn-link p-0 align-baseline" onclick={() => openJson(value)}>Open json data</button></li>
+                        {#if value?.latitude && value?.longitude}
+                            <li>Coordinate: {value.latitude}° N, {value.longitude}° E</li>
+                        {/if}
+                        <li>Water: {value.water.longname}</li>
+                    </ul>
 
-                <h3>Available Timeseries</h3>
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    {#each value?.timeseries as timeseries, i}
-                        <li class="nav-item" role="presentation">
-                            <button class="nav-link" class:active={i == 0} id="ts-{timeseries.shortname}-tab" data-bs-toggle="tab" data-bs-target="#ts-{timeseries.shortname}" type="button" role="tab" aria-controls={timeseries.shortname} aria-selected="true">{timeseries.shortname}</button>
-                        </li>
-                    {/each}
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    {#each value?.timeseries as timeseries, i}
-                        {@const allMarks = [
-                            {
-                                label: "Current Measurement",
-                                value: timeseries.currentMeasurement.value,
-                            },
-                            ...timeseries.characteristicValues.map((ts) => ({
-                                label: ts.longname,
-                                value: ts.value,
-                            })),
-                        ]}
-                        {@const allValues = allMarks.map((ts) => ts.value)}
-                        {@const valMin = Math.min(...allValues)}
-                        {@const valMax = Math.max(...allValues)}
-                        {@const valStep = niceStep(valMin, valMax, 10)}
+                    <h3>Available Timeseries</h3>
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        {#each value?.timeseries as timeseries, i}
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" class:active={i == 0} id="ts-{timeseries.shortname}-tab" data-bs-toggle="tab" data-bs-target="#ts-{timeseries.shortname}" type="button" role="tab" aria-controls={timeseries.shortname} aria-selected="true">{timeseries.shortname}</button>
+                            </li>
+                        {/each}
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        {#each value?.timeseries as timeseries, i}
+                            {@const allMarks = [
+                                {
+                                    label: "Current Measurement",
+                                    value: timeseries.currentMeasurement.value,
+                                },
+                                ...timeseries.characteristicValues.map((ts) => ({
+                                    label: ts.longname,
+                                    value: ts.value,
+                                })),
+                            ]}
+                            {@const allValues = allMarks.map((ts) => ts.value)}
+                            {@const valMin = Math.min(...allValues)}
+                            {@const valMax = Math.max(...allValues)}
+                            {@const valStep = niceStep(valMin, valMax, 10)}
 
-                        <div class="tab-pane" class:show={i == 0} class:active={i == 0} id="ts-{timeseries.shortname}" role="tabpanel" aria-labelledby="ts-{timeseries.shortname}-tab">
-                            <div class="d-flex flex-column">
-                                <ul>
-                                    <li>{timeseries.longname} in {timeseries.unit}; measured every {timeseries.equidistance} minutes</li>
-                                    {#if timeseries.currentMeasurement}
-                                        <li>
-                                            Last measurement:
-                                            {timeseries.currentMeasurement.value}
-                                            {timeseries.unit}
-                                            ({timeseries.currentMeasurement.timestamp})
-                                        </li>
-                                    {/if}
-                                </ul>
-                            </div>
+                            <div class="tab-pane" class:show={i == 0} class:active={i == 0} id="ts-{timeseries.shortname}" role="tabpanel" aria-labelledby="ts-{timeseries.shortname}-tab">
+                                <div class="d-flex flex-column">
+                                    <ul>
+                                        <li>{timeseries.longname} in {timeseries.unit}; measured every {timeseries.equidistance} minutes</li>
+                                        {#if timeseries.currentMeasurement}
+                                            <li>
+                                                Last measurement:
+                                                {timeseries.currentMeasurement.value}
+                                                {timeseries.unit}
+                                                ({timeseries.currentMeasurement.timestamp})
+                                            </li>
+                                        {/if}
+                                    </ul>
+                                </div>
 
-                            <div class="d-flex flex-row">
-                                <div>
-                                    {#if allValues.length > 1}
-                                        <Scale min={roundDownToN(valMin, valStep)} max={roundUpToN(valMax, valStep)} step={valStep} marks={allMarks} width={300} height={400} secondaryColor="#8080FF" fontSize="12" orientation="vertical" />
-                                    {/if}
-                                    <img src={getMeasurementsPngUrl(value.uuid, timeseries.shortname, {})} alt="Measurement URL" />
+                                <div class="d-flex flex-row">
+                                    <div>
+                                        {#if allValues.length > 1}
+                                            <Scale min={roundDownToN(valMin, valStep)} max={roundUpToN(valMax, valStep)} step={valStep} marks={allMarks} width={300} height={400} secondaryColor="#8080FF" fontSize="12" orientation="vertical" />
+                                        {/if}
+                                        <img src={getMeasurementsPngUrl(value.uuid, timeseries.shortname, {})} alt="Measurement URL" />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    {/each}
-                </div>
+                        {/each}
+                    </div>
+                {:else}
+                    <div>No data.</div>
+                {/if}
             {:catch error}
                 <p>Something went wrong: {error.message}</p>
             {/await}
