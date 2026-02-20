07cd3538a2efc162c33d22cf702b41045d0ca226
Alexander Kosnac <alexander.kosnac@web.de>
2026-02-20 19:46:54 +0100

    Refactor Kaprekar

--- src/routes/tool/kaprekar-constant/+page.svelte
+++ src/routes/tool/kaprekar-constant/+page.svelte
@@ -3,42 +3,43 @@
 
     let number = 8991;
 
-    $: run = runRoutine(number);
+    let kapIters = null;
+    let history = [];
+    let error = null;
 
-    function runRoutine(number) {
-        if (!number)
-            return;
+    const kaprekarRoutineWithHistory = (number) => kaprekarRoutine(number, (a, b, c) => history.push(`${a} - ${b} = ${c}`));
+
+    $: {
+        try {
+            history = [];
+            kapIters = kaprekarRoutineWithHistory(number);
+            error = null;
+        } catch (err) {
+            kapIters = null;
+            error = err.message;
+        }
+    }
+
+    function kaprekarRoutine(number, onIteration = (_a, _b, _c) => {}) {
+        if (!number || number < 1000 || number > 9999) return;
 
         let visited = [];
         let i = 0;
-        let err;
 
-        let history = [];
         let n = number;
         while (n != KAPREKAR_CONSTANT) {
             const k = kaprekarStep(n);
+            onIteration(k[0], k[1], k[2]);
+
             n = k[2];
-            history.push(`${k[0]} - ${k[1]} = ${k[2]}`);
 
-            if (visited.includes(n)) {
-                err = "Loop detected. Canceled.";
-                break;
-            } else {
-                visited.push(n);
-            }
+            if (visited.includes(n)) throw `Loop detected for ${number} in iteration ${i} with value ${n}.`;
+            if (i++ > 50) throw "Over 50 iterations.";
 
-            if (i++ > 1000) {
-                err = "Over 1000 iterations. Canceled.";
-                break;
-            }
+            visited.push(n);
         }
 
-        return {
-            number: number,
-            p: i,
-            history: history,
-            err: err,
-        };
+        return i;
     }
 
     function kaprekarStep(n) {
@@ -58,25 +59,25 @@
 <div class="row">
     <div class="col">
         <div class="input-group mb-2">
-            <button type="button" class="btn btn-primary" disabled={!number} onclick={runRoutine}>Run</button>
-            <input type="number" class="form-control" min="1" bind:value={number} placeholder="Number to evaluate" />
+            <button type="button" class="btn btn-primary" disabled={!number} onclick={() => kaprekarRoutineWithHistory(number)}>Run</button>
+            <input type="number" class="form-control" min="1000" max="9999" bind:value={number} placeholder="Number to evaluate" />
         </div>
+
+        {#if error}
+            (<span class="text-danger">{error}</span>)
+        {/if}
+
+        {#if kapIters}
+            <div>
+                {number} is a {kapIters}-Kaprekar number.
+            </div>
+
+            <div class="mt-4">
+                <h4>Calculations:</h4>
+                <pre>{#each history as step, i}#{i}:  {step}<br />{/each}</pre>
+            </div>
+        {/if}
     </div>
-
-    {#if run}
-        <div>
-            {run.number} is a {run.p}-Kaprekar number.
-
-            {#if run.err}
-                (<span class="text-danger">{run.err}</span>)
-            {/if}
-        </div>
-
-        <div class="mt-4">
-            <h4>Calculations:</h4>
-            <pre>{#each run.history as step, i}#{i}:  {step}<br />{/each}</pre>
-        </div>
-    {/if}
 </div>
 
 <style>
